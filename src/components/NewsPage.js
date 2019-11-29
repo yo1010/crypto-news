b51433288159b87813b99fba0c4787630ea78a61
Yavor Dimitrov <Yavor@yavor.local>
2019-11-29 13:51:51 +0200

    newsPage loading

--- src/components/NewsPage.js
+++ src/components/NewsPage.js
@@ -149,6 +149,7 @@ export default class NewsPage extends Component {
 NewsPage.contextType = ProductContext;
 
 const NewsPageWrapper = styled.div`
+    overflow-y: hidden;
     margin-top: 0.5rem;
     .notloaded {
         display: none;
